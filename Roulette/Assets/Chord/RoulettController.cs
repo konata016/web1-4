eb5846e2b7ff0c8f2f362165d45a1356030f49b2
Unknown <konata016@gmail.com>
2018-05-21 16:54:28 +0900

    ルーレットを動かす

---
+++ Roulette/Assets/Chord/RoulettController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoulettController : MonoBehaviour {
+
+    float rotSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //マウスが押されたらスピードオン
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        //回転速度分、ルーレットの回転
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //ルーレットを減速させる
+        this.rotSpeed *= 0.96f;
+	}
+}
